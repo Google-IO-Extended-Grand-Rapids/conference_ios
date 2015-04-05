1d7840b3794046a4eb82f8ccbc52fc1c4a64daea
Ryan McCracken <ryan.mccracken@sagetech-llc.com>
2015-04-04 22:15:19 -0400

    formatting custom cells

---
+++ ConferenceApp/ConferenceApp/EventCell.swift
@@ -0,0 +1,19 @@
+//
+//  EventCell.swift
+//  ConferenceApp
+//
+//  Created by Ryan McCracken on 4/4/15.
+//  Copyright (c) 2015 GR OpenSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    
+}
