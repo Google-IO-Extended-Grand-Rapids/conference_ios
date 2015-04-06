4c9179121f091c1c65f3077125e1f4490271fa24
Ryan McCracken <ryan.mccracken@sagetech-llc.com>
2015-04-06 16:38:36 -0400

    basic detail cells autolayout

---
+++ ConferenceApp/ConferenceApp/EventDetailCell.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailCell.swift
+//  ConferenceApp
+//
+//  Created by Ryan McCracken on 4/6/15.
+//  Copyright (c) 2015 GR OpenSource. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventDetailCell: UITableViewCell {
+    
+    
+    
+}
