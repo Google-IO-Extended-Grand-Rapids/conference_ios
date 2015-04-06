b31fe6b1039c22d194503cc23b84ff799a39c358
Ryan McCracken <ryan.mccracken@sagetech-llc.com>
2015-04-06 14:26:16 -0400

    basic detail page layout, top image, header

--- ConferenceApp/ConferenceApp/EventDetailsViewController.swift
+++ ConferenceApp/ConferenceApp/EventDetailsViewController.swift
@@ -51,19 +51,76 @@ class EventDetailsViewController: UITableViewController {
     // MARK: - Table View
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if (section == 0) {
+            return 1
+        } else {
+            return 3
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if (indexPath.section == 0) {
+            return 200
+        } else {
+            return 600
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("EventDetailCell", forIndexPath: indexPath) as UITableViewCell
         
-        let object = detailItem as Conference?
-        cell.textLabel!.text = object?.name
+        let object = detailItem as Conference!
+        
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCellWithIdentifier("BackgroundImageCell", forIndexPath: indexPath) as BackgroundImageCell
+            
+            if object.id! % 2 == 0 {
+                cell.backgroundImage.image = UIImage(named: "GRDark.jpg")
+            } else {
+                cell.backgroundImage.image = UIImage(named: "GRLight.jpg")
+            }
+        }
+        
+        cell.backgroundColor = .greenColor()
+        
         return cell
     }
     
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if (section == 1) {
+            return 80
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        let object = detailItem as Conference!
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        
+        let eventLabel = UILabel(frame: CGRectMake(8, 0, 592, 40))
+        eventLabel.text = object.name
+        eventLabel.textColor = UIColor .whiteColor()
+        headerView .addSubview(eventLabel)
+        
+        let durationLabel = UILabel(frame: CGRectMake(8, 40, 592, 40))
+        durationLabel.text = String(format: "%@ to %@",formatter.stringFromDate(object.startDate!),formatter.stringFromDate(object.endDate!))
+        durationLabel.textColor = UIColor .whiteColor()
+        durationLabel.font = UIFont .systemFontOfSize(12)
+        headerView .addSubview(durationLabel)
+        
+        headerView.backgroundColor = UIColor(red: 81.0/255.0, green: 94.0/255.0, blue: 193.0/255.0, alpha: 1)
+        
+        if (section == 1) {
+            return headerView
+        } else {
+            return nil
+        }
+    }
 }
